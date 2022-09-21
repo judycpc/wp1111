ad797b7be6b4b80a591ce37eb69e90263a58a77c
judycpc <abc0952753111@gmail.com>
2022-09-21 22:07:36 +0800

    hw2 init

---
+++ hw2/main.js
@@ -0,0 +1,10 @@
+
+// let attendees = document.getElementsByClassName("attendee");
+
+
+
+// attendees[0].onclick = () => {
+//     attendees[0].parentNode.removeChild(attendees[0])
+// }
+
+// console.log(attendees[0]);
