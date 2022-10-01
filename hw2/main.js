cbf6d7a4dae6bb761dd306c355bf7544894c7798
judycpc <abc0952753111@gmail.com>
2022-10-01 16:07:10 +0800

    requirement 1~3

--- hw2/main.js
+++ hw2/main.js
@@ -1,10 +1,27 @@
 
-// let attendees = document.getElementsByClassName("attendee");
+let attendees = document.getElementsByClassName("attendee");
+let anchored = document.getElementById("anchored");
+
+for (let i = 0; i < attendees.length; i++) {
+    const attendee = attendees[i];
+    
+    attendee.firstElementChild.firstElementChild.onclick = () => {
+        attendee.parentNode.removeChild(attendee);
+    }
+
+    attendee.children[1].lastElementChild.onclick = () => {
+        // swap name
+        let temp = attendee.lastElementChild.innerHTML;
+        attendee.lastElementChild.innerHTML = anchored.lastElementChild.lastElementChild.innerHTML;
+        anchored.lastElementChild.lastElementChild.innerHTML = temp;
+    
+        // swap pic
+        let temp2 = attendee.children[1].firstElementChild.outerHTML;
+        attendee.children[1].firstElementChild.outerHTML = anchored.children[1].firstElementChild.firstElementChild.outerHTML;
+        anchored.children[1].firstElementChild.firstElementChild.outerHTML = temp2;
+        attendee.children[1].firstElementChild.setAttribute('class', 'circle pic');
+        console.log(attendee.children[1].firstElementChild);
+    }    
+}
 
 
-
-// attendees[0].onclick = () => {
-//     attendees[0].parentNode.removeChild(attendees[0])
-// }
-
-// console.log(attendees[0]);
