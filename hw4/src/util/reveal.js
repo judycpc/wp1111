8037b45d1a1c3aae04036f1162d2f01b88ba08e9
judycpc <abc0952753111@gmail.com>
2022-10-12 02:13:28 +0800

    advanced requirement 2

--- hw4/src/util/reveal.js
+++ hw4/src/util/reveal.js
@@ -16,5 +16,24 @@ export const revealed = (newBoard, x, y, newNonMinesCount) => {
     //              If the value of the cell is 0, we should try to find the value of adjacent cells until the value we found is not 0.
     //              The input variables 'newNonMinesCount' and 'newBoard' may be changed in this function.
 
+    const offset = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]];
+
+    if (newBoard[x][y].value === 0) {
+      for (let i = 0; i < offset.length; i++) {
+        const bs = newBoard.length;
+        const pos = offset[i];
+        const _x = x + pos[0];
+        const _y = y + pos[1];
+        
+        if (_x >= 0 && _x < bs && _y >= 0 && _y < bs) {
+          if (!newBoard[_x][_y].revealed && !newBoard[_x][_y].flagged && !newBoard[_x][_y].value !== '💣') {
+            newBoard[_x][_y].revealed = true;
+            newNonMinesCount--;
+            ({ newBoard, newNonMinesCount } = revealed(newBoard, _x, _y, newNonMinesCount));
+          }
+        }
+      }
+    }
+
     return { newBoard, newNonMinesCount };
 };
