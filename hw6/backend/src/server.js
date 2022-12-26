c9f95badd738edfe516a3073dcbb1e5c460408a5
judycpc <abc0952753111@gmail.com>
2022-12-27 00:56:21 +0800

    modification for hw9

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -2,19 +2,30 @@ import express from 'express';
 import cors from 'cors';
 import db from './db';
 import routes from './routes';
+import path from 'path';
 
 db.connect();
 
 const app = express();
-const port = process.env.PORT || 4000;
 
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+    app.use(cors());
+}
 app.use(express.json());
 app.use('/', routes);
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'))
+    });
+}
 
 // app.get('/', (req, res) => {
 //     res.send('Hello, World!');
 // });
+const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
